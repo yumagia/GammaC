c97ea190685488a1e04ecaff3ccb8f1d96bfc58d
yumagia <gao00515@umn.edu>
2025-03-08 17:32:21 -0600

    Going through and adding more of Quake's command library

--- common/cmdlib.cpp
+++ common/cmdlib.cpp
@@ -6,19 +6,15 @@
 #include <direct.h>
 #include <windows.h>
 
-/*
-=================
-Error
-=================
-*/
-void Error (char *error, ...)
-{
+
+
+void Error(char *error, ...) {
 	va_list                 argptr;
 	char	                text[1024];
 	char	                text2[1024];
 	int		                err;
 
-	err = GetLastError ();
+	err = GetLastError();
 
 	va_start(argptr,error);
 	vsprintf(text, error,argptr);
@@ -29,3 +25,140 @@ void Error (char *error, ...)
 
 	exit(1);
 }
+
+/**
+ * =============================================================================
+ * 
+ * 		MISC FUNCTIONS
+ * 
+ * =============================================================================
+ */
+
+/**
+ * @brief Like pretty much all of GBSP's functions, this one is directly from Quake
+ */
+int Q_filelength(FILE *f) {
+	int		pos;
+	int		end;
+
+	pos = ftell(f);
+	fseek(f, 0, SEEK_END);
+	end = ftell(f);
+	fseek(f, pos, SEEK_SET);
+
+	return end;
+}
+
+
+FILE *SafeOpenWrite(char *filename)
+{
+	FILE	*f;
+
+	f = fopen(filename, "wb");
+
+	if (!f) {
+		Error ("Error opening %s: %s",filename,strerror(errno));
+	}
+
+	return f;
+}
+
+
+FILE	*SafeOpenRead(char *filename) {
+	FILE	*f;
+
+	f = fopen(filename, "rb");
+
+	if (!f) {
+		Error ("Error opening %s: %s",filename,strerror(errno));
+	}
+
+	return f;
+}
+
+
+void	SafeRead(FILE *f, void *buffer, int count)
+{
+	if (fread(buffer, 1, count, f) != (size_t)count) {
+		Error ("File read failure");
+	}
+}
+
+
+void	SafeWrite(FILE *f, void *buffer, int count) {
+	if (fwrite(buffer, 1, count, f) != (size_t)count) {
+		Error("File write failure");
+	}
+}
+
+
+bool	FileExists(char *filename) {
+	FILE	*f;
+
+	f = fopen(filename, "r");
+	if (!f) {
+		return false;
+	}
+	fclose (f);
+	return true;
+}
+
+
+int    LoadFile(char *filename, void **bufferptr) {
+	FILE	*f;
+	int    length;
+	void    *buffer;
+
+	f = SafeOpenRead(filename);
+	length = Q_filelength(f);
+	buffer = malloc(length+1);
+	((char *)buffer)[length] = 0;
+	SafeRead(f, buffer, length);
+	fclose(f);
+
+	*bufferptr = buffer;
+	return length;
+}
+
+
+/**
+ * @brief Allows failure
+ */
+int    TryLoadFile(char *filename, void **bufferptr) {
+	FILE	*f;
+	int    length;
+	void    *buffer;
+
+	*bufferptr = NULL;
+
+	f = fopen(filename, "rb");
+	if (!f) {
+		return -1;
+	}
+	length = Q_filelength(f);
+	buffer = malloc(length+1);
+	((char *)buffer)[length] = 0;
+	SafeRead(f, buffer, length);
+	fclose(f);
+
+	*bufferptr = buffer;
+	return length;
+}
+
+
+void    SaveFile(char *filename, void *buffer, int count)
+{
+	FILE	*f;
+
+	f = SafeOpenWrite(filename);
+	SafeWrite(f, buffer, count);
+	fclose(f);
+}
+
+/**
+ * =============================================================================
+ * 
+ * 		BYTE ORDER FUNCTIONS
+ * 
+ * =============================================================================
+ */
--- common/cmdlib.h
+++ common/cmdlib.h
@@ -8,3 +8,20 @@
 #include <stdarg.h>
 
 void    Error(char *error, ...);
+
+FILE	*SafeOpenWrite(char *filename);
+FILE	*SafeOpenRead(char *filename);
+void	SafeRead(FILE *f, void *buffer, int count);
+void	SafeWrite(FILE *f, void *buffer, int count);
+
+int		LoadFile(char *filename, void **bufferptr);
+int		TryLoadFile(char *filename, void **bufferptr);
+void	SaveFile(char *filename, void *buffer, int count);
+bool	FileExists(char *filename);
+
+short	BigShort (short l);
+short	LittleShort (short l);
+int		BigLong (int l);
+int		LittleLong (int l);
+float	BigFloat (float l);
+float	LittleFloat (float l);
