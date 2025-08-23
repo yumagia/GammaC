eac060327a160c15819f29c050516f9f64363a46
yumagia <gao00515@umn.edu>
2025-08-23 11:32:14 -0500

    Wrote the token functions

--- src/common/scriplib.cpp
+++ src/common/scriplib.cpp
@@ -95,6 +95,109 @@ bool GetToken(bool crossline) {
 
 	skipspace:
 		while(*script->script_p <= 32) {
-			
+			if(script->script_p >= script->end_p) {
+				return EndOfScript(crossline);
+			}
+			if(*script->script_p++ == '\n') {
+				if(!crossline) {
+					Error("Line %i is incomplete\n",scriptline);
+				}
+				scriptline = script->line++;
+			}
 		}
+
+		if(script->script_p >= script->end_p) {
+			return EndOfScript(crossline);
+		}
+
+		// Comments (; # //)
+		if(*script->script_p == ';' || *script->script_p == '#'
+			|| (script->script_p[0] == '/' && script->script_p[1] == '/')) {
+			if(!crossline) {
+				Error("Line %i is incomplete\n",scriptline);
+			}
+			while(*script->script_p++ != '\n') {
+				if(script->script_p >= script->end_p) {
+					return EndOfScript(crossline);
+				}
+			goto skipspace;
+			}
+		}
+
+		// Comments (/* */)
+		if(script->script_p[0] == '/' && script->script_p[1] == '*') {
+			if(!crossline) {
+				Error("Line %i is incomplete\n",scriptline);
+			}
+			script->script_p++;
+			if(script->script_p >= script->end_p) {
+				return EndOfScript(crossline);
+			}
+			script->script_p += 2;
+			goto skipspace;
+		}
+
+		// Copy token
+		token_p = token;
+
+		if(*script->script_p == '"') {		// Quoted token
+			script->script_p++;
+			while(*script->script_p != '"') {
+				*token_p++ = *script->script_p++;
+				if(script->script_p == script->end_p) {
+					break;
+				}
+				if(token_p == &token[MAXTOKEN]) {
+					Error("Token too large on line %i\n",scriptline);
+				}
+			}
+			script->script_p++;
+		}
+		else {								// Regular token
+			while(*script->script_p > 32 && *script->script_p != ';') {
+				*token_p++ = *script->script_p++;
+				if(script->script_p == script->end_p) {
+					break;
+				}
+				if(token_p == &token[MAXTOKEN]) {
+					Error("Token too large on line %i\n",scriptline);
+				}
+			}
+		}
+
+		*token_p = 0;
+
+		if(!(token == "$include")) {
+			GetToken(false);
+			AddScriptToStack(token);
+			return GetToken(crossline);
+		}
+
+		return true;
+}
+
+bool TokenAvailable(void) {
+	char	*search_p;
+
+	search_p = script->script_p;
+
+	if(search_p >= script->end_p) {
+		return false;
+	}
+
+	while(*search_p <= 32) {
+		if(*search_p == '\n') {
+			return false;
+		}
+		search_p++;
+		if(search_p == script->end_p) {
+			return false;
+		}
+	}
+
+	if(*search_p == ';') {
+		return false;
+	}
+
+	return true;
 }
