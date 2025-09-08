b3a17ceae07b5898f0ec0f512730f4f7d7ce4369
yumagia <gao00515@umn.edu>
2025-09-08 14:38:44 -0500

    Sharedlib

--- src/gamma/common/shared.h
+++ src/gamma/common/shared.h
@@ -1,22 +1,28 @@
+#pragma once
+
+#include <string>
 #include <math.h>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+
+
+
+/**=============================================
+ * MATHLIB
+ * 
+ * =============================================
+ */
 
 typedef float vec_t;
 typedef vec_t vec3_t[3];
 
-#define	SIDE_FRONT		0
-#define	SIDE_ON			2
-#define	SIDE_BACK		1
-#define	SIDE_CROSS		-2
-
-#define MAX_BOUND_DIM		99999
-
 #define	PI	                3.14159265358979323846
 
+struct plane_s;
+
 extern vec3_t vec3_origin;
-
-#define	EQUAL_EPSILON	0.001
-
-bool VectorCompare(vec3_t v1, vec3_t v2);
 
 #define DotProduct(x,y) (x[0]*y[0]+x[1]*y[1]+x[2]*y[2])
 #define VectorSubtract(a,b,c) {c[0]=a[0]-b[0];c[1]=a[1]-b[1];c[2]=a[2]-b[2];}
@@ -26,13 +32,23 @@ bool VectorCompare(vec3_t v1, vec3_t v2);
 #define VectorClear(x) {x[0] = x[1] = x[2] = 0;}
 #define	VectorNegate(x) {x[0]=-x[0];x[1]=-x[1];x[2]=-x[2];}
 
+bool VectorCompare(vec3_t v1, vec3_t v2);
 double VectorLength(vec3_t v);
-
 void VectorMA(vec3_t va, double scale, vec3_t vb, vec3_t vc);
-
 void CrossProduct(vec3_t v1, vec3_t v2, vec3_t cross);
 vec_t VectorNormalize(vec3_t in, vec3_t out);
 void VectorInverse(vec3_t v);
-
 void ClearBounds(vec3_t mins, vec3_t maxs);
 void AddPointToBounds(vec3_t v, vec3_t mins, vec3_t maxs);
+
+
+/**=============================================
+ * MATHLIB
+ * 
+ * =============================================
+ */
+
+typedef struct plane_s {
+	vec3_t		normal;
+	float		dist;
+} plane_t;
